e6a81fde2fcbc40a1cccbb8b3289734b05c15243
reshitnik <reshitnik@gmail.com>
2023-08-12 19:01:58 +0300

    sasas

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (matrix === undefined){
+    return [];
+  } else if (matrix.length === 0){
   return [];
+  }
+  let newArr = []
+  for (i=0; i<matrix.length; i++){
+    if (i%2==0){
+    let oddArr = matrix[i].sort((a,b) => a-b);
+    newArr = newArr.concat(oddArr);
+    }else {
+    let evenArr = matrix[i].sort((a,b) => b-a);
+    newArr = newArr.concat(evenArr);
+    };
+  };
+return newArr;
 }
+
